2c6aeade2f43faf697dcf571bbb47d4970aaeebb
coltonf <cfranklin@jhparch.com>
2020-10-23 09:30:20 -0500

    Ported to Archicad 22

--- .vscode/copyToAddons.ps1
+++ .vscode/copyToAddons.ps1
@@ -1,6 +1,6 @@
-Write-Output "Copying JHP_JHPeople.apx to Archicad 23 Add-On location..."
+Write-Output "Copying JHP_JHPeople.apx to Archicad 22 Add-On location..."
 $BuildLocation = "${PSScriptRoot}\..\Build\x64\Release\JHP_JHPeople.apx"
-$Destination = "C:\Program Files\GRAPHISOFT\ARCHICAD 23\Add-Ons\JHP\JHP_JHPeople.apx"
+$Destination = "C:\Program Files\GRAPHISOFT\ARCHICAD 22\Add-Ons\JHP\JHP_JHPeople.apx"
 
 Copy-Item -path $BuildLocation -Destination $Destination -Force
 
